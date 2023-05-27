48f03f33eab35e692644403fdc5778705b7aa9a3
Gabriel <gabrielliima.dev@gmail.com>
2023-05-27 18:50:39 -0300

    draw collision polygons and implement matter.js physics

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ import Map2 from "./scenes/map_2.js";
 
 const config = {
   type: Phaser.AUTO,
-  width: 2160,
-  height: 1440,
+  width: 3000,
+  height: 3000,
   backgroundColor: "#f9f9f9",
   physics: {
-    default: "arcade",
-    arcade: {
-      gravity: {
-        y: 0,
-      },
+    // default: "arcade",
+    // arcade: {
+    //   gravity: {
+    //     y: 0,
+    //   },
+    //   debug: true,
+    // },
+    default: "matter",
+    matter: {
+      gravity: { y: 1 },
+      enableSleep: false,
       debug: true,
     },
   },
