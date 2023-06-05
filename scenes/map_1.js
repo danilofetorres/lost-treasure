192ef06ed58a9889ca131ac2f5ff10189db86f90
Gabriel <gabrielliima.dev@gmail.com>
2023-06-05 20:12:12 -0300

    implement camera

--- scenes/map_1.js
+++ scenes/map_1.js
@@ -4,6 +4,8 @@ class Map1 extends Phaser.Scene {
   
   cursors;
   knight;
+  camera;
+  floor;
 
   constructor() {
     super("Map1");
@@ -11,6 +13,8 @@ class Map1 extends Phaser.Scene {
 
   init() {
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.camera = this.cameras.main;
+    this.floor = 0;
   }
 
   preload() {
@@ -19,7 +23,6 @@ class Map1 extends Phaser.Scene {
 
     // Load images
     this.load.image("tileset", "assets/tilesets/tileset.png");
-
 
     // Load characters
     this.load.atlas("knight", "../assets/character/knight/atlas/knight.png", "../assets/character/knight/atlas/knight.json");
@@ -33,7 +36,6 @@ class Map1 extends Phaser.Scene {
     // Create tiles
     const blocks = this.map.addTilesetImage("tileset", "tileset");
 
-    
     // Create layers
     const inner_background = this.map.createLayer("fundo_interno", blocks, 0, 0);
     const outside_background = this.map.createLayer("fundo", blocks, 0, 0);
@@ -45,19 +47,15 @@ class Map1 extends Phaser.Scene {
     const middle_ladder = this.map.createLayer("escada_meio", blocks, 0, 0);
     const windows = this.map.createLayer("janelas", blocks, 0, 0);
     const traps = this.map.createLayer("armadilhas", blocks, 0, 0);
-    const blockLayer = this.map.createLayer("blocklayer", blocks, 0, 0);
-
-    // Create camera
-    const camera = this.cameras.main;
-    camera.setBounds(0, 0, 2100, 400);
+    const blockLayer = this.map.createLayer("blocklayer", blocks, 0, 0);   
     
     // Set collision
     blockLayer.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(blockLayer);
-
+    
     traps.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(traps);
-
+    
     barrels.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(barrels);
     
@@ -68,7 +66,9 @@ class Map1 extends Phaser.Scene {
     this.knight.setFixedRotation();
     this.knight.depth = 1
 
-    camera.startFollow(this.knight, true, 0.08, 0.08);
+    // Set camera
+    this.camera.setBounds(0, 48, 2112, 480);
+    this.camera.startFollow(this.knight, true, 0.08, 0.08, 80);   
     
     // Create animations
     this.anims.create(cfg.anims(this, "idle", "knight", 14));
@@ -98,7 +98,25 @@ class Map1 extends Phaser.Scene {
 
     if(upJustPressed) {
       this.knight.setVelocityY(-7);
-    } 
+    }
+
+    // Camera transitions
+    if(this.knight.y > 0 && this.knight.y < 528 && this.floor != 0) {
+      this.camera.setBounds(0, 48, 2112, 480);
+      this.floor = 0;
+
+    } else if(this.knight.y >= 528 && this.knight.y < 912 && this.floor !== 1) {
+      this.camera.setBounds(0, 480, 2112, 480);
+      this.floor = 1;
+
+    } else if(this.knight.y >= 912 && this.knight.y < 1344 && this.floor !== 2) {
+      this.camera.setBounds(0, 910, 2112, 480);
+      this.floor = 2;
+
+    } else if(this.knight.y >= 1344 && this.knight.y < 1920 && this.floor !== 3) {
+      this.camera.setBounds(0, 1350, 2112, 570);
+      this.floor = 3;
+    }
   }
 }
 
